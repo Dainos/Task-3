189d110d1160679d27ca0e37123835de63eddb43
Dainos <cauton777@gmail.com>
2019-11-19 14:04:22 +0200

    Updates

---
+++ precache-manifest.d8c1fb01aba9a9f72976dbdb46b6ee0d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e4b32bbbfc6ceae4a9e45987fcb37aa",
+    "url": "/Task-3/index.html"
+  },
+  {
+    "revision": "3578d0372a3cd5add37c",
+    "url": "/Task-3/static/css/main.a6630368.chunk.css"
+  },
+  {
+    "revision": "85bbc279f6078feffac2",
+    "url": "/Task-3/static/js/2.31308ce9.chunk.js"
+  },
+  {
+    "revision": "3578d0372a3cd5add37c",
+    "url": "/Task-3/static/js/main.8041780a.chunk.js"
+  },
+  {
+    "revision": "071d48386c0d22b558fb",
+    "url": "/Task-3/static/js/runtime-main.1bc50e36.js"
+  }
+]);
